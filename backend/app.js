b7d5d92952d31293d3ea44d9b5de0ddf04f11890
zagoksu <zeynelgoksu@gmail.com>
2020-07-14 18:06:36 +0200

    base code before run build

--- backend/app.js
+++ backend/app.js
@@ -13,31 +13,26 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/uploads/images', express.static(path.join('uploads', 'images')));
-app.use(express.static(path.join('public')));
+app.use('/uploads/images', express.static(path.join('uploads', 'images')))
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//   );
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
-//   next();
-// });
+  next();
+});
 
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
 });
-
-// app.use((req, res, next) => {
-//   const error = new HttpError('Could not find this route.', 404);
-//   throw error;
-// });
 
 app.use((error, req, res, next) => {
   if (req.file) {
@@ -55,7 +50,7 @@ app.use((error, req, res, next) => {
 mongoose
     .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lac72.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
     .then(() => {
-      app.listen(process.env.PORT || 5000);
+      app.listen(5000);
     })
     .catch(err => {
       console.log(err);
